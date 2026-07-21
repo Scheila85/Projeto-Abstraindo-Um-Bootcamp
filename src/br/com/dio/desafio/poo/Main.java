eea5040af8418effbaaab4f51c83dc49cc36709c
scheila <sheillapaes08@gmail.com>
2026-07-21 10:41:40 -0300

    refactor: organizar a classe Main utilizando métodos auxiliares

--- src/br/com/dio/desafio/poo/Main.java
+++ src/br/com/dio/desafio/poo/Main.java
@@ -10,13 +10,17 @@ import br.com.dio.desafio.poo.dominio.Conteudos;
 import br.com.dio.desafio.poo.dominio.Developers;
 
 public class Main {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    private static Administrador administrador;
+    private static Developers developer;
+    private static Bootcamp bootcamp;
+
     public static void main(String[] args) {
-    
-        Scanner scanner = new Scanner(System.in);
-        Bootcamp bootcamp = null;
+
         int opcaoUsuario;
 
-        do{
+        while(true){
             System.out.println("\nEscolha o usuário:");
             System.out.println("1 - Administrador");
             System.out.println("2 - Developer");
@@ -27,18 +31,11 @@ public class Main {
             scanner.nextLine();
 
             switch (opcaoUsuario) {
-                case 1: 
+                case 1:
+                    cadastrarAdministrador();
+                    int opcaoMenuAdm = -2;
 
-                    Administrador administrador = null;
-                    int opcaoMenuAdm;
-
-                    System.out.println("\nCadastro de usuário");
-                    System.out.println("Digite seu nome: ");
-                    String nome = scanner.nextLine().trim();
-                    administrador = new Administrador(nome);
-                    System.out.println("Usuário criado com sucesso!");
-
-                    do {
+                    while (opcaoMenuAdm != 0) {
                         System.out.print("\n--Menu Administrator--\n");
                         System.out.print("1 - Criar Bootcamp\n");
                         System.out.print("2 - Adicionar cursos ao Bootcamp\n");
@@ -51,98 +48,21 @@ public class Main {
                         scanner.nextLine();
 
                         switch (opcaoMenuAdm) {
-                            case 1 -> {
-                                if (administrador == null) {
-                                    System.out.println("É necessário cadastrar um usuário, para criar bootcamps.");
-                                    break;
-                                }
-                                System.out.print("\nTítulo do Bootcamp: ");
-                                String tituloBootcamp = scanner.nextLine().trim();
-                                System.out.print("Descrição do Bootcamp: ");
-                                String descricaoBootcamp = scanner.nextLine().trim();
-                                bootcamp = administrador.criarBootcamp(tituloBootcamp,descricaoBootcamp);
-                                System.out.println("\nBootcamp, "+ tituloBootcamp + ", criado com sucesso!\n");
-                                }
-                            case 2 -> {
-                                if (bootcamp == null){
-                                    System.out.println("Crie um bootcamp para poder adicionar um curso.");
-                                    break;
-                                }
-                                System.out.print("\nTítulo do Curso: ");
-                                String tituloCurso = scanner.nextLine().trim();
-                                System.out.print("Descrição Curso: ");
-                                String descricaoCurso = scanner.nextLine().trim();
-                                System.out.print("Carga horária (digite apenas números): ");
-                                int cargaHoraria = scanner.nextInt();
-                                scanner.nextLine();
-                                Conteudos curso = administrador.adicionarCurso(tituloCurso, descricaoCurso, cargaHoraria, bootcamp);
-                                System.out.println("\nCurso "+ curso.getTitulo() + " criado com sucesso!");
-                                System.out.println("Curso adicionado ao bootcamp, "+ bootcamp.getName() + ", com sucesso!");
-                            }
-                            case 3 -> {
-                                if (bootcamp == null){
-                                    System.out.println("Crie um bootcamp para poder adicionar uma mentoria.");
-                                    break;
-                                }
-                                System.out.print("\nTítulo da Mentoria: ");
-                                String tituloMentoria= scanner.nextLine().trim();
-                                System.out.print("Descrição da Mentoria: ");
-                                String descricaoMentoria = scanner.nextLine().trim();
-                                System.out.print("Data ocorrência (ex 22/02/2026): ");
-                                String dataOcorrencia = scanner.nextLine();
-                                System.out.print("Hora acontecimento (ex 14:13): ");
-                                String horaAcontecimento = scanner.nextLine();
-
-                                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
-                                LocalTime horaAcontecimentoFormatted = LocalTime.parse(horaAcontecimento, formatter);
-                                Conteudos mentoria =  administrador.adicionarMentorias(tituloMentoria, descricaoMentoria, dataOcorrencia, horaAcontecimentoFormatted, bootcamp);
-                                System.out.println("\nMentoria "+ mentoria.getTitulo() + " criada com sucesso!");
-                                System.out.println("Mentoria adicionada ao bootcamp, "+ bootcamp.getName() + ", com sucesso!");
-                            }
-                            case 4 -> {
-                                if (bootcamp == null){
-                                    System.out.println("\nCrie um bootcamp para poder exibi-lo!");
-                                }else{
-                                    System.out.println("-------------------------");
-                                    System.out.println("Bootcamp: " + bootcamp.getName());
-                                    System.out.println("Descrição: " + bootcamp.getDescricao());
-                                    System.out.println("Data Início: " + bootcamp.getDataInicio());
-                                    System.out.println("Data Término: " + bootcamp.getDataTermino());
-                                    System.out.println("-------------------------");
-
-                                    System.out.println("Conteudos");
-                                    for (Conteudos conteudo : bootcamp.getConteudos()){
-                                        System.out.println(conteudo.retornaConteudo());
-                                    }
-                                    System.out.println("-------------------------");
-                                }
-                            }
-                            case 0 -> {
-                                System.out.println("\nVoltando ao menu inicial...");
-                            }
-                            default -> {
-                                System.out.println("\nOpção inválida, tente uma opção válida.\n");
-                            }
-                        }   
-                    } while (opcaoMenuAdm != 0);
-                    break;
+                            case 1 -> criarBootcamp();
+                            case 2 -> adicionarCursos();
+                            case 3 -> adicionarMentorias();
+                            case 4 -> visualizarBootcamp();
+                            case 0 -> System.out.println("\nVoltando ao menu inicial...");
+                            default -> System.out.println("\nOpção inválida, tente uma opção válida.\n");
+                        }
+                    }
+                break;
 
                 case 2:
-                    if (bootcamp == null) {
-                        System.out.println("\nNenhum bootcamp disponível no momento.");
-                        break;
-                    }
-                    Developers developer = null;
-                    int opcaoMenuDev;
-
                     System.out.println("\nSeja bem-vindo Dev!");
-
-                    System.out.println("\nCadastro de usuário");
-                    System.out.println("Digite seu nome: ");
-                    String nomeDev = scanner.nextLine();
-
-                    developer = new Developers(nomeDev);
-                    System.out.println("\nUsuário criado com sucesso!");
+                    cadastrarDeveloper();
+                    if (bootcampNaoInicializado()) break;
+                    int opcaoMenuDev;
 
                     do{
                         System.out.println("\n --Menu Developer--");
@@ -158,47 +78,154 @@ public class Main {
 
                         switch (opcaoMenuDev) {
                             case 1 -> {
-                                developer.seIncreverBootcamp(bootcamp);
-                                System.out.println("\nInscrição realizada com sucesso!");
-                                System.out.println("------------------------");
-                                    System.out.println("Bootcamp: " + bootcamp.getName());
-                                    System.out.println("Descrição: " + bootcamp.getDescricao());
-                                    System.out.println("Data Início: " + bootcamp.getDataInicio());
-                                    System.out.println("Data Término: " + bootcamp.getDataTermino());
-                                    System.out.println("---------------------");
-                                    System.out.println("Conteudos");
-                                    bootcamp.getConteudos().forEach(conteudo -> System.out.println(conteudo.retornaConteudo()));
-                                    System.out.println("---------------------");
+                                inscreverBootcamp();
+                                exibirInformacoesBootcamp();
                             }
-                            case 2 -> {
-                                developer.progredirBootcamp();
-                                System.out.println("\nParabéns, você finalizou mais um conteúdo!");
-                            }
-                            case 3 -> {
-                                System.out.printf("\nTotal xP: %s\n",developer.calcularXpAcumulada());
-                            }
-                            case 4 -> {
-                                developer.desistirBootcamp(bootcamp);
-                                System.out.println("\nVocê não está mais incrito no Bootcamp!");
-                            }
-                            case 0 -> {
-                                System.out.println("\nVoltando ao menu inicial...");
-                            }
-                            default -> {
-                                System.out.println("\nOpção inválida, tente uma opção válida.");
-                            }
+                            case 2 -> progredirBootcamp();
+                            case 3 -> calcularXp();
+                            case 4 -> desistirBootcamp();
+                            case 0 -> System.out.println("\nVoltando ao menu inicial...");
+                            default -> System.out.println("\nOpção inválida, tente uma opção válida.");
                         } 
                     } while (opcaoMenuDev != 0);
                 break;
 
-                case 0: 
+                case 0:
+                    scanner.close();
                     System.out.println("\nSistema encerrado.\n");
-                    break;
+                    System.exit(0);
                 default: 
                     System.out.println("\nOpção inválida, tente uma opção válida.");
             }
-        } while (opcaoUsuario != 0);
-    scanner.close();
+        }
+    }
+
+    private static void cadastrarAdministrador() {
+        System.out.println("\nCadastro de usuário");
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine().trim();
+        administrador = new Administrador(nome);
+        System.out.println("\nUsuário criado com sucesso!");
+    }
+
+    private static void criarBootcamp() {
+        if (administrador == null) {
+            System.out.println("É necessário cadastrar um usuário, para criar bootcamps.");
+            return;
+        }
+        System.out.print("\nTítulo do Bootcamp: ");
+        String tituloBootcamp = scanner.nextLine().trim();
+        System.out.print("Descrição do Bootcamp: ");
+        String descricaoBootcamp = scanner.nextLine().trim();
+
+        bootcamp = administrador.criarBootcamp(tituloBootcamp,descricaoBootcamp);
+        System.out.println("\nBootcamp, "+ tituloBootcamp + ", criado com sucesso!");
+    }
+
+    private static void adicionarCursos() {
+        if (bootcampNaoInicializado()){
+            System.out.println("\nCrie um bootcamp para poder adicionar um curso.");
+            return;
+        }
+        System.out.print("\nTítulo do Curso: ");
+        String tituloCurso = scanner.nextLine().trim();
+        System.out.print("Descrição Curso: ");
+        String descricaoCurso = scanner.nextLine().trim();
+        System.out.print("Carga horária (digite apenas números): ");
+        int cargaHoraria = scanner.nextInt();
+        scanner.nextLine();
+
+        Conteudos curso = administrador.adicionarCurso(tituloCurso, descricaoCurso, cargaHoraria, bootcamp);
+        System.out.println("Curso adicionado ao bootcamp, "+ bootcamp.getName() + ", com sucesso!");
+    }
+
+    private static void adicionarMentorias() {
+        if (bootcampNaoInicializado()) {
+            System.out.println("\nCrie um bootcamp para poder adicionar uma mentoria.");
+            return;
+        }
+        System.out.print("\nTítulo da Mentoria: ");
+        String tituloMentoria= scanner.nextLine().trim();
+        System.out.print("Descrição da Mentoria: ");
+        String descricaoMentoria = scanner.nextLine().trim();
+        System.out.print("Data ocorrência (ex 22/02/2026): ");
+        String dataOcorrencia = scanner.nextLine();
+        System.out.print("Hora acontecimento (ex 14:13): ");
+        String horaAcontecimento = scanner.nextLine();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        LocalTime horaAcontecimentoFormatted = LocalTime.parse(horaAcontecimento, formatter);
+
+        Conteudos mentoria =  administrador.adicionarMentorias(tituloMentoria, descricaoMentoria,
+                dataOcorrencia, horaAcontecimentoFormatted, bootcamp);
+        System.out.println("Mentoria adicionada ao bootcamp, "+ bootcamp.getName() + ", com sucesso!");
+    }
+
+    private static void visualizarBootcamp() {
+        if (bootcampNaoInicializado()){
+            System.out.println("\nCrie um bootcamp para poder exibi-lo!");
+            return;
+        }
+        exibirInformacoesBootcamp();
+    }
+
+    private static void cadastrarDeveloper() {
+        if (bootcampNaoInicializado()) {
+            System.out.println("\nNenhum bootcamp disponível no momento.");
+            return;
+        }
+        System.out.println("\nCadastro de usuário");
+        System.out.println("Digite seu nome: ");
+        String nomeDev = scanner.nextLine();
+
+        developer = new Developers(nomeDev);
+        System.out.println("\nUsuário criado com sucesso!");
+    }
+
+    private static void inscreverBootcamp() {
+        developer.seIncreverBootcamp(bootcamp);
+        System.out.println("\nInscrição realizada com sucesso!");
+    }
+
+    private static void progredirBootcamp() {
+        if (naoInscritoEmConteudos()){
+            System.out.println("\nVocê não está inscrito em nenhum conteúdo.");
+        }
+        developer.progredirBootcamp();
+        System.out.println("\nParabéns, você finalizou mais um conteúdo!");
+    }
+
+    private static void calcularXp() {
+        System.out.printf("\nTotal xP: %s\n",developer.calcularXpAcumulada());
+    }
+
+    private static void desistirBootcamp() {
+        if (naoInscritoEmConteudos()){
+            System.out.println("\nVocê não está inscrito em nenhum conteúdo.");
+        }
+        developer.desistirBootcamp(bootcamp);
+        System.out.println("\nVocê não está mais inscrito no Bootcamp!");
+    }
+
+    private static Boolean bootcampNaoInicializado(){
+        return bootcamp == null;
+    }
+
+    private static boolean naoInscritoEmConteudos(){
+        return developer.getConteudosInscritos() == null;
+    }
+
+    private static void exibirInformacoesBootcamp(){
+        System.out.println("------------------------");
+        System.out.println("Bootcamp: " + bootcamp.getName());
+        System.out.println("Descrição: " + bootcamp.getDescricao());
+        System.out.println("Data Início: " + bootcamp.getDataInicio());
+        System.out.println("Data Término: " + bootcamp.getDataTermino());
+        System.out.println("---------------------");
+        System.out.println("Conteúdos");
+        bootcamp.getConteudos().forEach(conteudo -> System.out.println
+                (conteudo.retornaConteudo()));
+        System.out.println("---------------------");
     }
 }
 
